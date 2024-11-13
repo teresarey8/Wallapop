2ba0eba245bc61a5c888699550a70e983caa5fd1
teresarey8 <teresareyagujetas@gmail.com>
2024-11-13 18:02:46 +0100

    Initial commit

---
+++ src/test/java/org/example/wallapop/WallapopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wallapop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallapopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
